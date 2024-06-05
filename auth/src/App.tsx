5b6468cba6cecd121a85b188b11908cc8b79ed96
Oleksii Budnik <budnikoleksii@gmail.com>
2024-06-05 16:40:21 +0300

    chore: init all microfrontends, setup linter

---
+++ auth/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div>AUTH</div>;
+}
+
+export default App;
---
+++ todo-form/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div>TODO FORM</div>;
+}
+
+export default App;
